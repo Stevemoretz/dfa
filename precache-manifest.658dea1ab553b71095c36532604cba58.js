f951fb916b7b4e450a6ea44e60df9455c8635f71
Stevemoretz <Sm.steve.moretz@gmail.com>
2020-06-06 16:16:22 +0430

    Add files via upload

---
+++ precache-manifest.658dea1ab553b71095c36532604cba58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f17dc10a99f50622ff2720ac0f88c7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfcdba1ccc9ec21a4578",
+    "url": "/static/css/main.0309d173.chunk.css"
+  },
+  {
+    "revision": "5f161bef2a5fe35d8dfb",
+    "url": "/static/js/2.42668284.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.42668284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfcdba1ccc9ec21a4578",
+    "url": "/static/js/main.a7960741.chunk.js"
+  },
+  {
+    "revision": "48b714112450bf41da9e",
+    "url": "/static/js/runtime-main.60473064.js"
+  }
+]);
